0ee2b4404e551d4eefa7bf996d5e4dd66c91bdde
emanuelvsz <usermane13@gmail.com>
2023-10-01 22:15:32 -0300

    updating folders schema

--- src/app/api/endpoints/dicontainer/handlers.go
+++
@@ -1,7 +0,0 @@
-package dicontainer
-
-import "keihi-kanri/src/app/api/endpoints/handlers"
-
-func GetUserHandlers() *handlers.UserHandlers {
-	return handlers.NewUserHandlers(GetUserServices())
-}
--- src/core/interfaces/primary/user_manager.go
+++
@@ -1,3 +0,0 @@
-package primary
-
-type UserManager interface{}
